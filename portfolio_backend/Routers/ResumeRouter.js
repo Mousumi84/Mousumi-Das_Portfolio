c603fa10a09b4c2cb76f9151e7bbcec6f96cd8cc
Mousumi Das <moutikdas8175@gmail.com>
2026-03-07 17:38:39 +0530

    fix

--- portfolio_backend/Routers/ResumeRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { saveResume } = require("../Controllers/ResumeController");
-
-const ResumeRouter = express.Router();
-
-ResumeRouter.post("/saveResume", saveResume);
-
-module.exports = ResumeRouter;
